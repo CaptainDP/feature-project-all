0e8e80be03ae798d37d15eff19775183fdaf4930
chendapeng <chendapeng@autohome.com.cn>
2021-05-11 15:26:30 +0800

    特征加工

--- feature_offline/src/main/scala/com/captain/bigdata/taichi/demo/app/UserClickSequenceApp.scala
+++ feature_offline/src/main/scala/com/captain/bigdata/taichi/demo/app/UserClickSequenceApp.scala
@@ -1,7 +1,11 @@
 package com.captain.bigdata.taichi.demo.app
 
+import com.alibaba.fastjson.JSON
+import com.captain.bigdata.taichi.util.DateUtil
+import com.google.gson.Gson
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.joda.time.DateTime
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -11,12 +15,128 @@ import scala.collection.mutable.ArrayBuffer
  */
 object UserClickSequenceApp {
 
+  val SERIES_IDS = "series_ids"
+  val BIZ_TYPE = "biz_type"
+  val AUTHOR_ID = "author_id"
+  val UNIQ_CATEGORY_NAME = "uniq_category_name"
+  val BRAND_IDS = "brand_ids"
+  val BRAND_APPLE = "APPLE"
+  val BRAND_HUAWEI = "HUAWEI"
+  val BRAND_OTHER = "OTHER"
+  val LIKE_COUNT = "like_count"
+  val REPLY_COUNT = "reply_count"
+  val param = """{"like_count":{"max":9911},"reply_count":{"max":6655}}""".stripMargin
+
   case class FeatureBean(device_id: String, biz_id: String, recommend_time: String, series_ids: String, biz_type: String, author_id: String, uniq_category_name: String, brand_ids: String, like_cnt_90d: String, reply_cnt_30d: String, device_brand: String, start_time: String, dt: String)
+
+  case class FeatureResultBean(device_id: String, biz_id: String, biz_type: String, publish_time: Double, match_series_weight: Double, match_series_click_idx_weight: Double, match_rtype_weight: Double, match_rtype_click_idx_weight: Double, match_author_weight: Double, match_author_click_idx_weight: Double, match_category_weight: Double, match_category_click_idx_weight: Double, match_brand_weight: Double, match_brand_click_idx_weight: Double, like_count: Double, reply_count: Double, device_brand_apple: Double, device_brand_huawei: Double, device_brand_other: Double, current_hour: Double)
 
   case class FeatureItemSeqBean(featureBean: FeatureBean, ItemSeqBean: Array[FeatureBean])
 
   def getString(x: FeatureBean): String = {
     x.dt + "," + x.device_id + "," + x.start_time + "," + x.biz_type + "," + x.biz_id + ","
+  }
+
+  def getTimeDecay(start: String, end: String): Double = {
+    val diff = DateUtil.getTimeDiff(end, start, "yyyy-MM-dd HH:mm:ss.S")
+    Math.exp(-0.0005 * diff / 600.0)
+  }
+
+  def getNormalized(like_count: String, expectType: String): Double = {
+
+    val json = JSON.parseObject(param)
+    if (json.getJSONObject(expectType) == null) {
+      0.0
+    } else {
+      val max = json.getJSONObject(expectType).getLongValue("max")
+
+      if (like_count.toLong >= max) {
+        1.0
+      } else if (max <= 0) {
+        0.0
+      } else {
+        1.0 * like_count.toLong / max
+      }
+    }
+
+  }
+
+  def getBrand(brand: String, expectBrand: String): Double = {
+    if (expectBrand.equalsIgnoreCase(brand)) {
+      1.0
+    } else {
+      0.0
+    }
+  }
+
+
+  def getHour(start_time: String): Double = {
+    val time = DateUtil.toDate(start_time, "yyyy-MM-dd HH:mm:ss.S")
+    val hour = new DateTime(time).getHourOfDay
+    Math.log10(hour) / Math.log10(24)
+  }
+
+  def getFieldValue(fieldName: String, featureBean: FeatureBean): String = {
+
+    fieldName match {
+      case SERIES_IDS => featureBean.series_ids
+      case BIZ_TYPE => featureBean.biz_type
+      case AUTHOR_ID => featureBean.author_id
+      case UNIQ_CATEGORY_NAME => featureBean.uniq_category_name
+      case BRAND_IDS => featureBean.brand_ids
+    }
+  }
+
+  def getMatchSeriesWeight(series_ids: String, itemSeqBean: Array[FeatureBean], fieldName: String): Double = {
+
+    var series_ids_list: Array[String] = null
+    if (series_ids != null) {
+      series_ids_list = series_ids.split(",")
+    }
+
+    if (series_ids_list.isEmpty) {
+      return 0.0
+    }
+
+    var count = 0
+    for (elem <- itemSeqBean) {
+      val tmp_series_ids = getFieldValue(fieldName, elem)
+      if (tmp_series_ids != null) {
+        val tmp_series_ids_list = tmp_series_ids.split(",")
+        if (tmp_series_ids_list.nonEmpty) {
+          count += (tmp_series_ids_list.toSet & series_ids_list.toSet).size
+        }
+      }
+    }
+
+    count / (series_ids_list.length * itemSeqBean.length + 0.01)
+  }
+
+  def getMatchSeriesClickIdxWeight(series_ids: String, itemSeqBean: Array[FeatureBean], fieldName: String): Double = {
+    var series_ids_list: Array[String] = null
+    if (series_ids != null) {
+      series_ids_list = series_ids.split(",")
+    }
+
+    if (series_ids_list.isEmpty) {
+      return 0.0
+    }
+
+    var i = 0
+    for (elem <- itemSeqBean) {
+      i += 1
+      val tmp_series_ids = getFieldValue(fieldName, elem)
+      if (tmp_series_ids != null) {
+        val tmp_series_ids_list = tmp_series_ids.split(",")
+        if (tmp_series_ids_list.nonEmpty) {
+          val count = (tmp_series_ids_list.toSet & series_ids_list.toSet).size
+          if (count > 0) {
+            return 1 - i / (itemSeqBean.length + 0.01)
+          }
+        }
+      }
+    }
+    0
   }
 
   def main(args: Array[String]): Unit = {
@@ -81,10 +201,39 @@ object UserClickSequenceApp {
       itemSeqList
     }).flatMap(x => x)
 
-    groupSeqRdd.collect().foreach(x => {
-      val tmp = getString(x.featureBean)
-      val tmpList = x.ItemSeqBean.map(x => getString(x)).mkString("#")
-      println(tmp + "$" + tmpList)
+    //加工
+    val featureResultRdd = groupSeqRdd.map(x => {
+
+      val featureBean = x.featureBean
+      val itemSeqBean = x.ItemSeqBean
+      val publish_time = getTimeDecay(featureBean.recommend_time, featureBean.start_time)
+      val match_series_weight = getMatchSeriesWeight(featureBean.series_ids, itemSeqBean, SERIES_IDS)
+      val match_series_click_idx_weight = getMatchSeriesClickIdxWeight(featureBean.series_ids, itemSeqBean, SERIES_IDS)
+      val match_rtype_weight = getMatchSeriesWeight(featureBean.biz_type, itemSeqBean, BIZ_TYPE)
+      val match_rtype_click_idx_weight = getMatchSeriesClickIdxWeight(featureBean.biz_type, itemSeqBean, BIZ_TYPE)
+      val match_author_weight = getMatchSeriesWeight(featureBean.author_id, itemSeqBean, AUTHOR_ID)
+      val match_author_click_idx_weight = getMatchSeriesClickIdxWeight(featureBean.author_id, itemSeqBean, AUTHOR_ID)
+      val match_category_weight = getMatchSeriesWeight(featureBean.uniq_category_name, itemSeqBean, UNIQ_CATEGORY_NAME)
+      val match_category_click_idx_weight = getMatchSeriesClickIdxWeight(featureBean.uniq_category_name, itemSeqBean, UNIQ_CATEGORY_NAME)
+      val match_brand_weight = getMatchSeriesWeight(featureBean.brand_ids, itemSeqBean, BRAND_IDS)
+      val match_brand_click_idx_weight = getMatchSeriesClickIdxWeight(featureBean.brand_ids, itemSeqBean, BRAND_IDS)
+      val like_count = getNormalized(featureBean.like_cnt_90d, LIKE_COUNT)
+      val reply_count = getNormalized(featureBean.reply_cnt_30d, REPLY_COUNT)
+      val device_brand_apple = getBrand(featureBean.device_brand, BRAND_APPLE)
+      val device_brand_huawei = getBrand(featureBean.device_brand, BRAND_HUAWEI)
+      val device_brand_other = if (device_brand_apple > 0 || device_brand_huawei > 0) 0 else 1
+      val current_hour = getHour(featureBean.start_time)
+
+      FeatureResultBean(featureBean.device_id, featureBean.biz_id, featureBean.biz_type, publish_time, match_series_weight,
+        match_series_click_idx_weight, match_rtype_weight, match_rtype_click_idx_weight, match_author_weight, match_author_click_idx_weight,
+        match_category_weight, match_category_click_idx_weight, match_brand_weight, match_brand_click_idx_weight,
+        like_count, reply_count, device_brand_apple, device_brand_huawei, device_brand_other, current_hour)
+    })
+
+    featureResultRdd.collect().foreach(x => {
+      val gson = new Gson()
+      val tmp = gson.toJson(x)
+      println(tmp)
     })
 
     spark.stop()

8fad87de5280b94d58a50be5a6b7a0d07ce9439c
chendapeng <chendapeng@autohome.com.cn>
2021-10-15 19:38:55 +0800

    ctr和时长合并发送

--- feature_offline/src/main/scala/com/captain/bigdata/taichi/demo/app/BeidouExperimentReport.scala
+++ feature_offline/src/main/scala/com/captain/bigdata/taichi/demo/app/BeidouExperimentReport.scala
@@ -8,8 +8,12 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import taichi.utils.AutoMessage
 
+import scala.collection.mutable.ArrayBuffer
+
 
 object BeidouExperimentReport {
+
+  case class Result(title: String, dt: String, trial_id: String, duizhaoCtr: String, shiyanCtr: String, diffCtr: String, var duizhaoDuration: String, var shiyanDuration: String, var diffDuration: String)
 
   def main(args: Array[String]): Unit = {
 
@@ -54,20 +58,19 @@ object BeidouExperimentReport {
     ctr_query_sql = ctr_query_sql.replaceAll("exps", exps)
     var df = spark.sql(ctr_query_sql)
 
-    var ctr_msg = "ctr:\n实验\t日期\t对照桶\t实验桶\tctr涨幅\n"
     var i = 0
     var ctrtmp = 0.0
+    val list = ArrayBuffer[Result]()
     df.collect().foreach(x => {
       if (i % 2 == 1) {
-        val diff = (x.get(3).toString.toDouble - ctrtmp).formatted("%.2f") + "%"
-        val line = x.get(0) + "\t" + x.get(1) + "\t" + ctrtmp + "%\t" + x.get(3) + "%\t" + diff + "\n"
-        ctr_msg += line
+        val diff = (x.get(3).toString.toDouble - ctrtmp).formatted("%.2f")
+        val result = Result(x.get(0).toString, x.get(1).toString, x.get(2).toString, ctrtmp + "%", x.get(3) + "%", diff + "%", "", "", "")
+        list.append(result)
       } else {
         ctrtmp = x.get(3).toString.toDouble
       }
       i += 1
     })
-    println("ctr_msg2:" + ctr_msg)
 
 
     //--------------------------------------------------------------------------------------------
@@ -87,24 +90,33 @@ object BeidouExperimentReport {
 
     duration_query_sql = duration_query_sql.replaceAll("exps", exps)
     df = spark.sql(duration_query_sql)
-    var duration_msg = "时长:\n实验\t日期\t对照桶\t实验桶\t时长涨幅\n"
     var durationtmp = 0.0
     i = 0
     df.collect().foreach(x => {
       if (i % 2 == 1) {
         val diff = (x.get(3).toString.toDouble - durationtmp).formatted("%.2f")
-        val line = x.get(0) + "\t" + x.get(1) + "\t" + durationtmp + "\t" + x.get(3) + "\t" + diff + "\n"
-        duration_msg += line
+        val result = list((i - 1) / 2)
+        if (result.title.equals(x.get(0).toString) && result.dt.equals(x.get(1).toString) && result.trial_id.equals(x.get(2))) {
+          result.duizhaoDuration = durationtmp + ""
+          result.shiyanDuration = x.get(3).toString
+          result.diffDuration = diff
+        }
       } else {
         durationtmp = x.get(3).toString.toDouble
       }
       i += 1
     })
-    println("duration_msg:" + duration_msg)
+
+    var msg = "ctr和时长：\n实验\t日期\t对照桶ctr\t实验桶ctr\tctr涨幅\t对照桶时长\t实验桶时长\t时长涨幅\n"
+
+    list.foreach(x => {
+      val line = x.title + "\t\t" + x.dt + "\t\t" + x.duizhaoCtr + "\t\t" + x.shiyanCtr + "\t\t" + x.diffCtr + "\t\t" + x.duizhaoDuration + "\t\t" + x.shiyanDuration + "\t\t" + x.diffDuration + "\n"
+      msg += line
+    })
 
     val users = "13830,11592,14325,14810,15333"
-    val msg = ctr_msg + "\n\n" + duration_msg
     AutoMessage.send("北斗实验ctr和时长效果数据", msg, users, "email")
+    AutoMessage.send("北斗实验ctr和时长效果数据", "邮件已发送请查收", users, "ding")
 
     spark.stop()
   }
